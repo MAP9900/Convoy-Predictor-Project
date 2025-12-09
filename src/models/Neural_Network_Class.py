fc489b903e8bfa45322d7c3f31165fe4f17d13ca
Matthew Plambeck <142714965+MAP9900@users.noreply.github.com>
2025-12-08 20:01:16 -0500

    Remove Neural Network File (No longer using)

--- src/models/Neural_Network_Class.py
+++
@@ -1,7 +0,0 @@
-#Imports
-
-
-
-#TODO: Create a base neural network class which can be used to expirenment
-#  with the neural networks and thier effectiveness. Should have a strong level 
-# of custimization in order to try varoius techniques and optimization methods.  
